f64286abae9c0c34ae234c1f7893e4a99deab0aa
Hryhorii <grigorijgul0@gmail.com>
2025-09-23 16:37:53 +0300

    first branch-02 commit

---
+++ PR-2/PR-2.cpp
@@ -0,0 +1,25 @@
+﻿
+
+// Lab_02.1.cpp
+// < Гуль Григорій>
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 5
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double m; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	z1 = 1-(0.25*(sin(2*m)*sin(2*m)))+cos(2*m);
+	//z2 = (cos(m)* cos(m)) + (cos(m) * cos(m)*cos(m) * cos(m));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
