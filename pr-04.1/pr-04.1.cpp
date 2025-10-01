4aba946c61c332494e706409ecfac2ea6d3f7ea0
Hryhorii <grigorijgul0@gmail.com>
2025-10-01 10:44:01 +0300

    lab04.1

---
+++ pr-04.1/pr-04.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int i;
+	double P;
+	P = 1;
+	i = 1;
+	while (i <= 15)
+	{
+		double numerator = pow(sin(i), 2) + pow(cos(1.0 / i), 2);
+		double denominator = pow(i, 2);
+		P *= numerator / denominator;
+		i++;
+	}
+	cout << P << endl;
+	P = 1;
+	i = 1;
+	do {
+		P *= (pow(sin(i), 2) + pow(cos(1.0 / i), 2)) / pow(i, 2);
+		i++;
+	} while (i <= 15);
+	cout << P << endl;
+	P = 1;
+	for (i = 1; i <= 15; i++)
+	{
+		P *= (pow(sin(i), 2) + pow(cos(1.0 / i), 2)) / pow(i, 2);
+	}
+	cout << P << endl;
+	P = 1;
+	for (i = 15; i >= 1; i--)
+	{
+		P *= (pow(sin(i), 2) + pow(cos(1.0 / i), 2)) / pow(i, 2);
+	}
+	cout << P << endl;
+	return 0;
+}
