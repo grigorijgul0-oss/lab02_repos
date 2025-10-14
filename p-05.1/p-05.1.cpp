7de26c8457e0dd3a42c96f0be9cba43f332befb5
Hryhorii <grigorijgul0@gmail.com>
2025-10-14 19:29:31 +0300

    lab-05.1

---
+++ p-05.1/p-05.1.cpp
@@ -0,0 +1,21 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double h(const double s, const double t); // прототип
+
+int main()
+{
+	double a, b;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	double c = (a / (pow(b, 2) + 1)) + (1 / pow(a, 2) + pow(a, 2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double s, const double t) // визначення
+{
+	return (pow(h(s, t), 4) + h(1, pow(s, 2) + pow(t, 2))) / (1 + pow(h(s * t, 1), 2));
+}
