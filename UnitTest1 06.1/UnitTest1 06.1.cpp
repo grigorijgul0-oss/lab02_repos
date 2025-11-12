9805b0f9611315aa16f56ac8d6e8ccfc10305d96
Hryhorii <grigorijgul0@gmail.com>
2025-11-12 09:31:29 +0200

    6.1(it)

---
+++ UnitTest1 06.1/UnitTest1 06.1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr-06.1/pr-06.1.cpp"  // шлях до твоєї програми
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestIterative
+{
+	TEST_CLASS(UnitTestIterative)
+	{
+	public:
+
+		TEST_METHOD(TestCountPositiveEven)
+		{
+			int arr[] = { -3, 2, 4, 5, 6, 0, -8 };
+			int result = CountPositiveEven(arr, 7);
+			Assert::AreEqual(3, result); // 2,4,6
+		}
+
+		TEST_METHOD(TestSumPositiveEven)
+		{
+			int arr[] = { -3, 2, 4, 5, 6, 0, -8 };
+			int result = SumPositiveEven(arr, 7);
+			Assert::AreEqual(12, result); // 2+4+6
+		}
+
+		TEST_METHOD(TestReplacePositiveEvenWithZero)
+		{
+			int arr[] = { -3, 2, 4, 5, 6, 0, -8 };
+			ReplacePositiveEvenWithZero(arr, 7);
+			int expected[] = { -3, 0, 0, 5, 0, 0, -8 };
+			for (int i = 0; i < 7; i++)
+				Assert::AreEqual(expected[i], arr[i]);
+		}
+	};
+}
---
+++ pr-06.1/pr-06.1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int SIZE = 25;
+const int MIN_VAL = -20;
+const int MAX_VAL = 30;
+
+
+void GenerateArray(int arr[], int size, int minVal, int maxVal) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = minVal + rand() % (maxVal - minVal + 1);
+    }
+}
+
+void PrintArray(const int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << setw(4) << arr[i];
+    cout << endl;
+}
+
+int CountPositiveEven(const int arr[], int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++)
+        if (arr[i] > 0 && arr[i] % 2 == 0)
+            count++;
+    return count;
+}
+
+int SumPositiveEven(const int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        if (arr[i] > 0 && arr[i] % 2 == 0)
+            sum += arr[i];
+    return sum;
+}
+
+void ReplacePositiveEvenWithZero(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        if (arr[i] > 0 && arr[i] % 2 == 0)
+            arr[i] = 0;
+}
+
+int main() {
+    srand((unsigned)time(0));
+
+    int a[SIZE];
+
+  
+    GenerateArray(a, SIZE, MIN_VAL, MAX_VAL);
+
+    cout << "start masiv:" << endl;
+    PrintArray(a, SIZE);
+
+    
+    int count = CountPositiveEven(a, SIZE);
+    int sum = SumPositiveEven(a, SIZE);
+
+    
+    cout << "\ncount: " << count << endl;
+    cout << "sum: " << sum << endl;
+
+    
+    ReplacePositiveEvenWithZero(a, SIZE);
+
+    cout << "\nmodufikovanij masiv" << endl;
+    PrintArray(a, SIZE);
+
+    return 0;
+}
