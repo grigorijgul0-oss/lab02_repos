51280811af96cb26da77d3fa66b5fc268164a76d
Hryhorii <grigorijgul0@gmail.com>
2025-11-12 09:42:46 +0200

    06.2(it)

---
+++ UnitTest06.2(it)/UnitTest06.2(it).cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr-06.2(it)/pr-06.2(it).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestIterative
+{
+	TEST_CLASS(UnitTestIterative)
+	{
+	public:
+
+		TEST_METHOD(TestAverageOdd_Positive)
+		{
+			int a[] = { 1, 2, 3, 4, 5 };
+			Assert::AreEqual(3.0, averageOdd(a, 5), 0.001);
+		}
+
+		TEST_METHOD(TestAverageOdd_AllEven)
+		{
+			int a[] = { 2, 4, 6, 8 };
+			Assert::AreEqual(0.0, averageOdd(a, 4), 0.001);
+		}
+
+		TEST_METHOD(TestAverageOdd_NegativeValues)
+		{
+			int a[] = { -5, -4, -3, -2, -1 };
+			Assert::AreEqual(-3.0, averageOdd(a, 5), 0.001);
+		}
+	};
+}
---
+++ pr-06.2(it)/pr-06.2(it).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void createArray(int a[], int n, int minVal, int maxVal) {
+    srand(time(0));
+    for (int i = 0; i < n; i++)
+        a[i] = minVal + rand() % (maxVal - minVal + 1);
+}
+
+void printArray(const int a[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << setw(5) << a[i];
+    cout << endl;
+}
+
+double averageOdd(const int a[], int n) {
+    int sum = 0, count = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 != 0) {
+            sum += a[i];
+            count++;
+        }
+    }
+    return (count == 0) ? 0 : (double)sum / count;
+}
+
+int main() {
+    const int n = 10;
+    int a[n];
+
+    createArray(a, n, -10, 10);
+
+    cout << "masiv:" << endl;
+    printArray(a, n);
+
+    double avg = averageOdd(a, n);
+    cout << fixed << setprecision(2);
+    cout << "avg = " << avg << endl;
+
+    return 0;
+}
